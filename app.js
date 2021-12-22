e1cbc0d67b7223767a3ff015804cdb89f2158cee
Niayshin <shavnick.main@gmail.com>
2021-12-22 18:49:14 +0300

    update cors

--- app.js
+++ app.js
@@ -32,13 +32,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(mainRoute)
 
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 async function start() {
     try {
         await mongoose.connect(mongoDB, {
